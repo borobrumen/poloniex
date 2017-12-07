8e51fa92fb43de1b25264a101943bc52904baebb
Boro Brumen <bbrumen@cervomedia.com>
2017-12-07 13:49:42 +0100

    parsing error protection for ticker data

--- modules/trading/data/stock.js
+++ modules/trading/data/stock.js
@@ -11,15 +11,38 @@ module.exports = {
   low_24h:0,
 
   setData: function(pair_name, dataObj) {
-    this.name = pair_name;
-    this.last = Number(dataObj["last"]);
-    this.lowestAsk = Number(dataObj["lowestAsk"]);
-    this.highestBid = Number(dataObj["highestBid"]);
-    this.percentChange = Number(dataObj["percentChange"]);
-    this.baseVolume = Number(dataObj["baseVolume"]);
-    this.quoteVolume = Number(dataObj["quoteVolume"]);
-    this.isFrozen = Boolean(dataObj["isFrozen"]);
-    this.high_24h = Number(dataObj["high24hr"]);
-    this.low_24h = Number(dataObj["low24hr"]);
+    try {
+      this.name = pair_name;
+      if(dataObj["last"] != undefined){
+        this.last = Number(dataObj["last"]);
+      }
+      if(dataObj["lowestAsk"] != undefined){
+        this.lowestAsk = Number(dataObj["lowestAsk"]);
+      }
+      if(dataObj["highestBid"] != undefined){
+        this.highestBid = Number(dataObj["highestBid"]);
+      }
+      if(dataObj["percentChange"] != undefined){
+        this.percentChange = Number(dataObj["percentChange"]);
+      }
+      if(dataObj["baseVolume"] != undefined){
+        this.baseVolume = Number(dataObj["baseVolume"]);
+      }
+      if(dataObj["quoteVolume"] != undefined){
+        this.quoteVolume = Number(dataObj["quoteVolume"]);
+      }
+      if(dataObj["isFrozen"] != undefined){
+        this.isFrozen = Boolean(dataObj["isFrozen"]);
+      }
+      if(dataObj["high24hr"] != undefined){
+        this.high_24h = Number(dataObj["high24hr"]);
+      }
+      if(dataObj["low24hr"] != undefined){
+        this.low_24h = Number(dataObj["low24hr"]);
+      }
+
+    } catch (e) {
+      console.console.warn("parsing error on: dataObj:" + dataObj + " -->(stock.js/setData)");
+    }
   }
 }
